7d09fea7b8bb330ee43a29ee323c08b472b1cfad
Armor <1669487366@qq.com>
2019-12-11 22:45:49 +0800

    fix

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+const port = Number(process.env.PORT || 3099);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port, () => {
+    console.log(`Listening  at http://localhost:${port}`);
+  });
 }
 bootstrap();
